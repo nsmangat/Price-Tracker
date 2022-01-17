744ed35e558804870330ece7fb4d3a25c0d22f41
Navdeep Mangat <Nmangat0337@conestogac.on.ca>
2022-01-16 22:35:37 -0500

    Added user input to search for item

--- PriceScraper.py
+++ PriceScraper.py
@@ -5,13 +5,20 @@ url = "https://www.amazon.ca/"
 # browser = webdriver.Chrome(executable_path = "C:\Users\nsman\Desktop\Amazon Price Scraper")
 # browser = webdriver.Chrome()
 # browser.get(url)
+#driver = webdriver.Chrome(executable_path = 'C:\Users\nsman\Desktop\Amazon Price Scraper')
+
+getSearch = input("Please type in an item ")
 
 driver = webdriver.Chrome()
 driver.get(url)
 
-getSearch = input("Please type in an item ")
+
+search = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+search.clear()
+search.send_keys(getSearch)
+search.submit()
 
 
 driver.close()
 
-#check
+
