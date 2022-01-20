66b21ab20123fe68fc686783ae9e4eff72a6b4bc
Navdeep Mangat <Nmangat0337@conestogac.on.ca>
2022-01-19 20:22:09 -0500

    Added more scraping info

--- PriceScraper.py
+++ PriceScraper.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from datetime import date
+import mysql.connector
+
+#username: Scraper
+#password: Pythonscraper
 
 #things to add:
 #add user input for item, automate to search for it and grab url, display current price
@@ -7,14 +12,27 @@ from selenium.webdriver.common.keys import Keys
 
 url = "https://www.amazon.ca/PlayStation-4-Dual-Charging-Dock/dp/B00ENFVJJO/ref=sr_1_10?crid=2FQ1OYX6BS5AJ&keywords=playstation+4&qid=1642393254&sprefix=play%2Caps%2C136&sr=8-10"
 #url = "https://www.amazon.ca/Gusseted-Quilted-Pillow-Hypo-Allergenic/dp/B01N4WAERJ/ref=sr_1_5?crid=3CIGG4PDDSD81&keywords=pillow&qid=1642535283&sprefix=pillow%2Caps%2C131&sr=8-5"
-item = "PlayStation 4 Dual Charging Dock"
+
+# scraper_db = mysql.connector.connect(
+#     host = "localhost",
+#     user = "Scraper",
+#     password = "Pythonscraper",
+#     database = "scraper"
+# )
+
+# cursor = scraper_db.cursor()
+
 
 driver = webdriver.Chrome()
 driver.get(url)
 
 #price = (driver.find_element_by_xpath('//*[@id="corePrice_desktop"]/div/table/tbody/tr/td[2]/span[1]/span[2]')).text
 #price = driver.find_element_by_id("corePrice_desktop").text
+item = driver.find_element_by_id("productTitle").text
 price = driver.find_element_by_id("corePrice_feature_div").text
+rating = driver.find_element_by_id("acrPopover").get_attribute("title")
+num_of_ratings = driver.find_element_by_id("acrCustomerReviewText").text
+
 driver.close()
 
 if price.find('\n') != -1:
@@ -22,7 +40,19 @@ if price.find('\n') != -1:
     price = price_split[0] + '.' + price_split[1]
 
 
+rating_split = rating.split(" ")
+rating = rating_split[0] + "/5"
+
+run_date = date.today()
+
+print(run_date)
+print(item)
 print(price)
+print(rating)
+print(num_of_ratings)
+
+# cursor.execute("CREATE TABLE " + item + " (")
+# scraper_db.close()
 
 
 
